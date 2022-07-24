07895696e337729ffaabb2540e821778bdc85c12
Tiffany Dempsey <tiffany.dempsey2000@gmail.com>
2022-07-23 21:57:58 -0700

    refactors searchbook file to use mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,11 +1,67 @@
 import { gql } from '@apollo/client';
 
+export const DELETE_BOOK = gql`
+mutation deleteBook($bookId: ID!) {
+    deleteBook(bookId: $bookId) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            titile
+            description
+            link
+            image
+        }
+    }
+}`;
+
+export const SAVE_BOOK = gql`
+mutations saveBook($newBook: InputBook!) {
+    saveBook(newBook:$newBook) {
+        _id
+        username
+        email
+        title
+        description
+        link
+        image
+        authors
+    }
+}`;
+
+export const USER_LOGIN = gql`
+mutation login( $email: String! $password: String!) {
+    login(email:$email password:$password) {
+        token user {
+            _id
+            username
+        }
+    }
+}`;
+
 export const CREATE_USER = gql`
+mutation createUser($username: String! $email: String! $password: String!) {
+    createUser (username:$username email: $email password: $password) {
+        token user {
+            _id
+            username
+            email
+            bookCount
+            savedBooks {
+                authors
+                bookId
+                image
+                link
+                title
+                description
+            }
+        }
+    }
+}`;
 
 
-`;
 
-export const SEARCH_BOOK = gql`
- 
-  
-`;
+
+
--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,57 +2,14 @@ const { Book, User } = require("../models");
 
 const resolvers = {
   Query: {
-    getSingleUser: async ({ user = null, params }, res) => {
-      const foundUser = await User.findOne({
-        $or: [
-          { _id: user ? user._id : params.id },
-          { username: params.username },
-        ],
-      });
-      if (!foundUser) {
-        return res
-          .status(400)
-          .json({ message: "Cannot find a user with this id!" });
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select('-__v -password');
+        return userData;
       }
-      res.json(foundUser);
+      throw new AuthenticationError('You need to be logged in!');
     },
-    createUser: async ({ body }, res) => {
-      const user = await User.create(body);
-      if (!user) {
-        return res.status(400).json({ message: "Something is wrong!" });
-      }
-      const token = signToken(user);
-      res.json({ token, user });
-    },
-    login: async ({ body }, res) => {
-      const user = await User.findOne({
-        $or: [{ username: body.username }, { email: body.email }],
-      });
-      if (!user) {
-        return res.status(400).json({ message: "Can't find this user" });
-      }
-      const correctPw = await user.isCorrectPassword(body.password);
-
-      if (!correctPw) {
-        return res.status(400).json({ message: "Wrong password!" });
-      }
-      const token = signToken(user);
-      res.json({ token, user });
-    },
-    saveBook: async ({ user, body }, res) => {
-      console.log(user);
-      try {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: user._id },
-          { $addToSet: { savedBooks: body } },
-          { new: true, runValidators: true }
-        );
-        return res.json(updatedUser);
-      } catch (err) {
-        console.log(err);
-        return res.status(400).json(err);
-      }
-    },
+  
 
   },
   Mutation: {
@@ -66,6 +23,43 @@ const resolvers = {
           return res.status(404).json({ message: "Couldn't find user with this id!" });
         }
         return res.json(updatedUser);
+      },
+      createUser: async ({ body }, res) => {
+        const user = await User.create(body);
+        if (!user) {
+          return res.status(400).json({ message: "Something is wrong!" });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+      },
+      login: async ({ body }, res) => {
+        const user = await User.findOne({
+          $or: [{ username: body.username }, { email: body.email }],
+        });
+        if (!user) {
+          return res.status(400).json({ message: "Can't find this user" });
+        }
+        const correctPw = await user.isCorrectPassword(body.password);
+  
+        if (!correctPw) {
+          return res.status(400).json({ message: "Wrong password!" });
+        }
+        const token = signToken(user);
+        res.json({ token, user });
+      },
+      saveBook: async ({ user, body }, res) => {
+        console.log(user);
+        try {
+          const updatedUser = await User.findOneAndUpdate(
+            { _id: user._id },
+            { $addToSet: { savedBooks: body } },
+            { new: true, runValidators: true }
+          );
+          return res.json(updatedUser);
+        } catch (err) {
+          console.log(err);
+          return res.status(400).json(err);
+        }
       },
   },
 };
